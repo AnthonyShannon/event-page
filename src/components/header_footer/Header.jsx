2057362dafccc931c16e4da94f26004aeb3b6891
ashan <ashannon1990@gmail.com>
2019-11-18 21:00:24 -0500

    created appbar

--- src/components/header_footer/Header.jsx
+++ src/components/header_footer/Header.jsx
@@ -7,8 +7,18 @@ class Header extends Component {
             <div>
                 <AppBar 
                     position="fixed"
+                    style={{
+                        backgroundColor: '#2f2f2f',
+                        boxShadow: 'none',
+                        padding: "10px, 0px"
+                    }}
                 >
-                    Header
+                    <Toolbar>
+                        <div className="header_">
+                            <div className="font_righteous header_logo_venue">The Venue</div>
+                            <div className="header_logo_title">Musical Events</div>
+                        </div>
+                    </Toolbar>
                 </AppBar>
             </div>
         );
