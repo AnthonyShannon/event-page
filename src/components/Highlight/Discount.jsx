bec46a8a408859b121a5ef9d75d4f91d90228799
ashan <ashannon1990@gmail.com>
2019-11-21 21:48:00 -0500

    added animations to highlight section

--- src/components/Highlight/Discount.jsx
+++ src/components/Highlight/Discount.jsx
@@ -2,19 +2,45 @@ import React, { Component } from 'react';
 import { Fade, Slide } from 'react-reveal';
 
 class Discount extends Component {
+
+    state = {
+        discountStart: 0,
+        discountEnd: 30
+    }
+
+    percentage = () => {
+        if (this.state.discountStart < this.state.discountEnd) {
+            this.setState({
+                discountStart: this.state.discountStart + 1
+            })
+        }
+    }
+
+    componentDidUpdate(){
+        setTimeout(() => {
+            this.percentage()
+        },30)
+    }
+
+
     render() {
         return (
             <div className="center_wrapper">
                 <div className="discount_wrapper">
-                    <div className="discount_percentage">
-                        <span>23%</span>
-                        <span>OFF</span>
-                    </div>
-
-                    <div className="discount_description">
-                        <h3>Purchase tickets before December 1st</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae iure ex iste animi ratione praesentium non, dolorem accusamus reprehenderit quasi repudiandae voluptate, tempora quam vitae corporis quas inventore incidunt mollitia.</p>
-                    </div>
+                    <Fade 
+                        onReveal={() => this.percentage()}
+                    >
+                        <div className="discount_percentage">
+                            <span>{this.state.discountStart}%</span>
+                            <span>OFF</span>
+                        </div>
+                    </Fade>
+                    <Slide right>
+                        <div className="discount_description">
+                            <h3>Purchase tickets before December 1st</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae iure ex iste animi ratione praesentium non, dolorem accusamus reprehenderit quasi repudiandae voluptate, tempora quam vitae corporis quas inventore incidunt mollitia.</p>
+                        </div>
+                    </Slide>
 
                 </div>
             </div>
